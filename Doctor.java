86df3bfd662c5c521c7ee62dc747bd5c935a45ae
Jonnier Angel Condori Catasi <jcondoricata@unsa.edu.pe>
2025-10-15 22:32:38 -0500

    Add availability field to Doctor class

--- Doctor.java
+++ Doctor.java
@@ -1,23 +1,24 @@
 package javaapplication45;
 
-
 public class Doctor {
     private String codigo;
     private String nombre;
     private String especialidad;
+    private boolean disponible; // true = disponible, false = no disponible
 
-
-    public Doctor(String codigo, String nombre, String especialidad) {
+    public Doctor(String codigo, String nombre, String especialidad, boolean disponible) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.especialidad = especialidad;
-
+        this.disponible = disponible;
     }
 
     public String getCodigo() { return codigo; }
     public String getNombre() { return nombre; }
     public String getEspecialidad() { return especialidad; }
+    public boolean isDisponible() { return disponible; }
 
-
-    
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
