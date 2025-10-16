84857a0c883f5a688643f51e464cb7213946d4ee
Jonnier Angel Condori Catasi <jcondoricata@unsa.edu.pe>
2025-10-15 22:14:05 -0500

    .
    Removed working hours from Doctor class and adjusted constructor.

--- Doctor.java
+++ Doctor.java
@@ -6,27 +6,19 @@ public class Doctor {
     private String codigo;
     private String nombre;
     private String especialidad;
-    private LocalTime horaInicio;
-    private LocalTime horaFin;
 
-    public Doctor(String codigo, String nombre, String especialidad, LocalTime horaInicio, LocalTime horaFin) {
+
+    public Doctor(String codigo, String nombre, String especialidad) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.especialidad = especialidad;
-        this.horaInicio = horaInicio;
-        this.horaFin = horaFin;
+
     }
 
     public String getCodigo() { return codigo; }
     public String getNombre() { return nombre; }
     public String getEspecialidad() { return especialidad; }
 
-    public boolean estaDisponible(LocalTime hora) {
-        return !hora.isBefore(horaInicio) && !hora.isAfter(horaFin);
-    }
 
-    @Override
-    public String toString() {
-        return String.format("[%s] %s - %s (%s–%s)", codigo, nombre, especialidad, horaInicio, horaFin);
-    }
+    
 }
